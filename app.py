5bb66ee284c15a0ae84b4548280080f1936391da
Ravisen22 <69089604+Ravisen22@users.noreply.github.com>
2020-08-07 18:37:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = str(name) + "likes to play games"
+	new_name = str(name) + " likes to play games"
 	return render_template('index.html', new_name=name)
 
 
